7ec64ef217a7d5962ab613934471efad4231d1ea
Enno Rehling <enno.rehling@gmail.com>
2010-09-29 21:53:08 -0700

    attempt to reduce lag in sound

--- sound.js
+++ sound.js
@@ -12,28 +12,48 @@ var NUM_SFX = 6;
 
 var sfx_name = ["jump", "land", "death", "spring", "splash", "fly", "bump" ];
 var sounds = [];
+var sfx_extension;
 
 function play_sound(sfx, loop) {
-    if (!main_info.music_no_sound) {
-        var audio = document.createElement('audio');
-        var format = audio.canPlayType('audio/mpeg')?'mp3':'ogg';
-        audio.addEventListener('canplaythrough', function(ev) {
-            this.removeEventListener('canplaythrough', arguments.callee,false);
-            this.play();
-        }, false);
-        audio.src = "sound/" + sfx_name[sfx] + "." + format;
-        if (loop) audio.loop = true;
-        audio.load();
-        for (var i in sounds) {
-            if (sounds[i].ended) {
-                sounds[i] = audio;
-                audio = null;
-                break;
+    if (main_info.music_no_sound) {
+        return;
+    }
+    var i, replace = -1;
+    var audio;
+    for (i in sounds) {
+        if (sounds[i].audio.ended) {
+            replace = i;
+            if (sounds[i].sfx == sfx) {
+                audio = sounds[i].audio;
+                audio.play();
+                return;
             }
         }
-        if (audio) {
-            sounds.push(audio);
+    }
+    if (replace>=0) {
+        audio = sounds[i].audio;
+    } else {
+        audio = document.createElement('audio');
+    }
+    sfx_extension = audio.canPlayType('audio/mpeg')?'mp3':'ogg';
+    audio.addEventListener('canplaythrough', function(ev) {
+        this.removeEventListener('canplaythrough', arguments.callee,false);
+        this.play();
+    }, false);
+    audio.src = "sound/" + sfx_name[sfx] + "." + sfx_extension;
+    if (loop) audio.loop = true;
+    audio.load();
+    for (var i in sounds) {
+        if (sounds[i].ended) {
+            sounds[i] = audio;
+            audio = null;
+            break;
         }
+    }
+    if (replace>=0) {
+        sounds[replace] = { audio: audio, sfx: sfx };
+    } else {
+        sounds.push({ audio: audio, sfx: sfx });
     }
 }
 
