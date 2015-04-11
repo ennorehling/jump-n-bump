c5d3cb3a1a2fab4cb8afbdf5251cafc233e9c662
GrahamTheCoder <GrahamTheCoder@gmail.com>
2015-04-11 13:14:55 +0100

    Add spaces for readability

--- game/levels/default/level.js
+++ game/levels/default/level.js
@@ -12,16 +12,17 @@ var ban_map;
 
 function GET_BAN_MAP_XY(x, y) {
     if (y<0) y = 0;
-    return ban_map[(x>>SCALE_FACTOR)+(y>>SCALE_FACTOR)*LEVEL_WIDTH];
+    return ban_map[(x >> SCALE_FACTOR) + (y >> SCALE_FACTOR) * LEVEL_WIDTH];
 }
 
 function GET_BAN_MAP(x, y) {
-    if (y<0) y = 0;
-    return ban_map[x+y*LEVEL_WIDTH];
+    if (y < 0) y = 0;
+    return ban_map[x + y * LEVEL_WIDTH];
 }
 
 function GET_BAN_MAP_IN_WATER(s1, s2) {
-    return (GET_BAN_MAP_XY((s1), ((s2) + 7)) == BAN_VOID || GET_BAN_MAP_XY(((s1) + 15), ((s2) + 7)) == BAN_VOID) && (GET_BAN_MAP_XY((s1), ((s2) + 8)) == BAN_WATER || GET_BAN_MAP_XY(((s1) + 15), ((s2) + 8)) == BAN_WATER);
+    return (GET_BAN_MAP_XY((s1), ((s2) + 7)) == BAN_VOID || GET_BAN_MAP_XY(((s1) + 15), ((s2) + 7)) == BAN_VOID)
+	&& (GET_BAN_MAP_XY((s1), ((s2) + 8)) == BAN_WATER || GET_BAN_MAP_XY(((s1) + 15), ((s2) + 8)) == BAN_WATER);
 }
 	
 function create_map() {
@@ -43,7 +44,7 @@ function create_map() {
         "2222222214000001333111" +
         "1111111111111111111111";
     ban_map = [];
-    for (i=0; i!= LEVEL_WIDTH * LEVEL_HEIGHT; ++i) {
-        ban_map[i] = levelmap[i]*1; //Convert to integer
+    for (i=0; i != LEVEL_WIDTH * LEVEL_HEIGHT; ++i) {
+        ban_map[i] = levelmap[i] * 1; //Convert to integer
     }
 }
